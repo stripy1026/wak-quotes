e7ec50440e594d36766ff99da876d6f337b4db0c
stripy1026 <aequatio1026@gmail.com>
2023-05-23 14:27:27 +0900

    feat: :sparkles: add form init
    need to refactor img to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,12 @@ export default function Home() {
     <>
       <h1>WAK QUOTES</h1>
       <Image src="/quote_template.png" width={800} height={450} alt="quote" />
-      <div>type quote form here</div>
+      <form>
+        <label>
+          <strong>우왁굳이 말하게 하고 싶은 명언은 ?</strong>
+        </label>
+        <textarea maxLength={80} />
+      </form>
     </>
   );
 }
