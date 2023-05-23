01a16a1f7471d624847a13a5780953e3da48ca3a
stripy1026 <aequatio1026@gmail.com>
2023-05-23 14:22:18 +0900

    feat: :bento: add quote template img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1>WAK-QUOTES</h1>
-      <div>WAK quote image here</div>
+      <h1>WAK QUOTES</h1>
+      <Image src="/quote_template.png" width={800} height={450} alt="quote" />
       <div>type quote form here</div>
     </>
   );
