fda1cc30f08cf7b0d65ee5ed52b5f6c2343ea52d
stripy1026 <aequatio1026@gmail.com>
2023-05-23 14:51:33 +0900

    feat: :sparkles: form now works
    type message to submit quote

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
+import { FormEvent, useState } from "react";
+
 import { QuoteTemplate } from "@/components/QuoteTemplate";
-import Image from "next/image";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [quote, setQuote] = useState("");
+
+  const addQuoteMessage = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuote(message);
+  };
+
   return (
     <>
       <h1>WAK QUOTES</h1>
-      <QuoteTemplate quote="깨지고 부숴져라" />
-      <form>
+      <QuoteTemplate quote={quote} />
+      <form onSubmit={addQuoteMessage}>
         <label>
           <strong>우왁굳이 말하게 하고 싶은 명언은 ?</strong>
         </label>
-        <textarea maxLength={80} />
+        <textarea
+          className="bg-slate-700"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          maxLength={80}
+        />
+        <button type="submit">입력</button>
       </form>
     </>
   );
