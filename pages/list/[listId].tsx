2bd817e42f74971a335cad788e422e7a9dcad7a2
stripy1026 <inwoo1026@naver.com>
2023-05-27 01:03:23 +0900

    feat: :sparkles: replace console log to html element

--- pages/list/[listId].tsx
+++ pages/list/[listId].tsx
@@ -2,10 +2,10 @@ import { useRouter } from "next/router";
 
 export default function ListId() {
   const router = useRouter();
-  console.log("LISTID: ", router.query.listId);
   return (
     <>
       <h3>This is ListId page</h3>
+      <div>ID: {router.query.listId}</div>
     </>
   );
 }
