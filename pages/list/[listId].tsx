b749db3565645a541fe2e68eaccd676c00fb3d16
stripy1026 <inwoo1026@naver.com>
2023-05-27 01:02:11 +0900

    feat: :loud_sound: listid page query log

--- pages/list/[listId].tsx
+++ pages/list/[listId].tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 
 export default function ListId() {
   const router = useRouter();
-  console.log(router);
+  console.log("LISTID: ", router.query.listId);
   return (
     <>
       <h3>This is ListId page</h3>
