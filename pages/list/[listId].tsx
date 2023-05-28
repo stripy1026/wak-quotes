72ee08d4299378681aa576b61a4ead5a8d384289
stripy1026 <inwoo1026@naver.com>
2023-05-28 13:48:14 +0900

    feat: :sparkles: listId get props as quote for rendering

--- pages/list/[listId].tsx
+++ pages/list/[listId].tsx
@@ -1,19 +1,24 @@
+import { QuoteTemplate } from "@/components/QuoteTemplate";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
-export default function ListId() {
+type ListIdProps = {
+  quote: string;
+};
+
+export default function ListId({ quote }: ListIdProps) {
   const router = useRouter();
   return (
     <>
       <h3>This is ListId page</h3>
-      <div>ID: {router.query.listId}</div>
+      <QuoteTemplate quote={quote} />
     </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  console.log(ctx);
+  console.log(ctx.query.listId);
   return {
-    props: {},
+    props: { quote },
   };
 };
