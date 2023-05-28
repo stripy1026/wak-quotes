6193c4e4acc7ed0e14501dd0433d345b51d0f535
stripy1026 <inwoo1026@naver.com>
2023-05-28 15:05:07 +0900

    feat: :sparkles: add List props to quote array

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,7 +1,42 @@
-export default function List() {
+import { GetServerSideProps } from "next";
+
+import clientPromise from "@/lib/mongodb";
+
+type ListProps = {
+  quotes: string[];
+};
+
+export default function List({ quotes }: ListProps) {
   return (
     <>
       <h1> This is List Page</h1>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const client = await clientPromise;
+  const db = client.db(process.env.DB_NAME);
+
+  const quotes = await db
+    .collection(process.env.QUOTES_COLLECTION_NAME as string)
+    .find()
+    .toArray();
+
+  if (quotes === null) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      quotes: quotes.map((quote) => ({
+        message: quote.message,
+      })),
+    },
+  };
+};
