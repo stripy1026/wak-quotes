677d5933bc917940fdb027b4d20030260bb82472
stripy1026 <aequatio1026@gmail.com>
2023-05-29 17:23:05 +0900

    style: :lipstick: layout done
    position fixed

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <nav className="bg-gray-900 py-4">
+      <nav className="bg-gray-900 p-4 fixed top-0 left-0 right-0 z-10">
         <ul className="flex justify-center space-x-6">
           <li>
             <Link className="text-white hover:text-gray-300" href="/">
