a0586a3f8d48ee7082055567bb7dc4e27d8d0deb
stripy1026 <aequatio1026@gmail.com>
2023-06-23 16:04:44 +0900

    feat: :sparkles: search form init

--- pages/ranking/index.tsx
+++ pages/ranking/index.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { FormEvent, useState } from "react";
 
 type ListProps = {
   quotes: {
@@ -20,8 +21,17 @@ export default function Ranking({ quotes }: ListProps) {
   const router = useRouter();
   const { user, error, isLoading } = useUser();
 
+  const [message, setMessage] = useState("");
+  const [searchOption, setSearchOption] = useState("quote");
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
+  const handleSearchQuotes = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Selected option value:", searchOption);
+  };
 
   const handlePostLikesQuote = async (quoteId: string) => {
     try {
@@ -43,6 +53,31 @@ export default function Ranking({ quotes }: ListProps) {
 
   return (
     <div className="p-4">
+      <form className="flex mb-2" onSubmit={handleSearchQuotes}>
+        <textarea
+          className="flex-1 bg-slate-700 w-full h-10 mx-2 mt-2 p-2 rounded"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          maxLength={80}
+          placeholder="1-80자 검색"
+        />
+        <select
+          className="flex-none bg-slate-700 mt-2 mr-2"
+          name="search"
+          id="ranking-search"
+          value={searchOption}
+          onChange={(e) => setSearchOption(e.target.value)}
+        >
+          <option value="quote">명언</option>
+          <option value="nick">작성자</option>
+        </select>
+        <button
+          type="submit"
+          className="flex-none px-4 py-2 mt-2 bg-blue-500 text-white rounded hover:bg-blue-600 float-right"
+        >
+          입력
+        </button>
+      </form>
       <ul>
         {quotes.map((quote) => (
           <div className="mb-4 relative" key={quote.id}>
