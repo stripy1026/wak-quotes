506104f185ec49302657f7152e1ea782d3418998
stripy1026 <aequatio1026@gmail.com>
2023-07-04 00:17:49 +0900

    feat: :sparkles: change nickname update date

--- pages/api/postUserNickname.ts
+++ pages/api/postUserNickname.ts
@@ -37,6 +37,7 @@ export default async function handler(
       $set: {
         auth0Id: userSession.user.sub,
         nickname: message,
+        dateNicknameChanged: new Date(),
       },
     },
     {
